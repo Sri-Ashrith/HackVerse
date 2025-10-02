774aae3c9eaecd8b060d949e392399cc89227971
Chintapalli Sri Ashrith <sriashrithwork@gmail.com>
2025-10-02 23:56:32 +0530

    Add files via upload

---
+++ files.bpcontent.cloud/2025/09/28/12/20250928124930-9ZF5TFPX.js
@@ -0,0 +1,31 @@
+window.botpress.init({
+  "botId": "218d1c28-49e5-4ef1-a725-85ae65605def",
+  "configuration": {
+    "version": "v2",
+    "botName": "HackVerse Support Agent",
+    "botAvatar": "https://files.bpcontent.cloud/2025/10/02/14/20251002143044-IEIE8PW8.png",
+    "botDescription": "I’m here to help you with any questions or support requests related to HackVerse 1.0. Ask me about event details, registration, amenities, or anything else!",
+    "website": {
+      "title": "https://sri-ashrith.github.io/HackVerse/",
+      "link": "https://sri-ashrith.github.io/HackVerse/"
+    },
+    "email": {
+      "title": "Email",
+      "link": "hackverserec@gmail.com"
+    },
+    "phone": {},
+    "termsOfService": {},
+    "privacyPolicy": {},
+    "color": "#B05CFC",
+    "variant": "solid",
+    "headerVariant": "glass",
+    "themeMode": "dark",
+    "fontFamily": "inter",
+    "radius": 4,
+    "feedbackEnabled": false,
+    "footer": "[⚡ by Botpress](https://botpress.com/?from=webchat)",
+    "soundEnabled": false,
+    "toggleChatId": "bp-toggle-chat"
+  },
+  "clientId": "1195b4b3-8c26-4b20-9214-d77a9c300aa6"
+});
